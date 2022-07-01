df8aa8397f2dfbb2f22b232dd9a3817ee4bbc69c
Saeed Adam <sandeed70@gmail.com>
2022-07-01 11:15:58 +0300

    Add project files.

---
+++ TacoCatMVC/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using TacoCatMVC.Models;
+
+namespace TacoCatMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Reverse()
+        {
+            Palindrome palindrome = new Palindrome();
+            return View(palindrome);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Reverse(Palindrome palindrome)
+        {
+            string inputWord = palindrome.InputWord;
+            string revWord = string.Empty;
+
+
+            if (!string.IsNullOrEmpty(inputWord))
+            {
+                for (int i = inputWord.Length - 1; i >= 0; i--)
+                {
+                    revWord += inputWord[i];
+                }
+            }
+
+            palindrome.RevWord = revWord;
+
+            revWord = Regex.Replace(revWord.ToLower(), "[^a-zA-Z0-9]+", "");
+            inputWord = Regex.Replace(inputWord.ToLower(), "[^a-zA-Z0-9]+", "");
+
+            if (revWord == inputWord)
+            {
+                palindrome.IsPalindrome = true;
+                palindrome.Message = $"Well done! {palindrome.InputWord} is a palindrome.";
+            }
+            else
+            {
+                palindrome.IsPalindrome = false;
+                palindrome.Message = $"Sorry! ({palindrome.InputWord}) is not a palindrome.";
+            }
+            
+            return View(palindrome);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TacoCatMVC/Models/Palindrome.cs
@@ -0,0 +1,10 @@
+﻿namespace TacoCatMVC.Models
+{
+    public class Palindrome
+    {
+        public string InputWord { get; set; }
+        public string RevWord { get; set; }
+        public bool IsPalindrome { get; set; }
+        public string Message { get; set; }
+    }
+}
